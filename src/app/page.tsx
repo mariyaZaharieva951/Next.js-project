74dba7caca089a3c06a01cb60a258d525ec283f0
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-05-03 08:10:59 +0300

    Add Marketing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Benefits from "@/components/layout/Benefits/Benefits";
 import Header from "@/components/layout/Header/Header";
 import Hero from "@/components/layout/Hero/Hero";
 import Sales from "@/components/layout/Hero/Salex";
+import Marketing from "@/components/layout/Marketing/Marketing";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Benefits/>
       <div className="mt-[120px]">
         <Sales/>
+        <Marketing/>
       </div>
       
     </main>
