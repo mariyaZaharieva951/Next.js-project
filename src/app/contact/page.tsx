b218a72eb47c85a9b375898fcf9a2014ce1766f7
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-05-16 00:14:01 +0300

    Update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -25,7 +25,14 @@ const Contact = () => {
               <p className="lg:text-lg text-base text-gray-darked pb-5">
                 Plovdiv, Bulgaria, bul Vasil Levski 96
               </p>
-              <img src="./assets/map.png" />
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d23661.304981853667!2d24.73369452710239!3d42.157495207563706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x14acd18de452a06b%3A0x838e93baa43989e5!2z0KHQtdCy0LXRgNC10L0sINGD0LsuIOKAntCS0LDRgdC40Lsg0JvQtdCy0YHQutC44oCcIDk2LCA0MDAzINCf0LvQvtCy0LTQuNCy!3m2!1d42.1625957!2d24.743444399999998!5e0!3m2!1sbg!2sbg!4v1714642166594!5m2!1sbg!2sbg"
+                width="458"
+                height="288"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+              ></iframe>
               <div className="flex justify-between pt-5">
                 <div className="border-l-2 border-gray-darked pl-2">
                   <p className="font-bold lg:text-lg text-base">General</p>
@@ -79,7 +86,7 @@ const Contact = () => {
         </div>
       </div>
 
-      <Clients/>
+      <Clients />
       <div className="pb-[100px]"></div>
       <Footer />
     </div>
